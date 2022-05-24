c7e1cb76800f20558211d81a6de2de1ea2942587
Gustavo Endringer Pereira <82174986+Theendringer@users.noreply.github.com>
2022-05-24 17:20:04 -0300

    Add files via upload

---
+++ Cine/script.js
@@ -0,0 +1,14 @@
+
+function salvarDados(){
+    const nomeAgendamento = document.getElementById("agendamentoNome")
+    const nome = nomeAgendamento.value
+    console.log(nome)
+}
+
+
+function teste(){
+    console.log("teste")
+}
+
+console.log("teste")
+
